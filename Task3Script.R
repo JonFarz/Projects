2e2e895371e35e475e3e0acce7f184fbb214c459
JonFarz <jfarzanfar1@gmail.com>
2015-02-22 12:55:18 -0500

    Futher Work on Task 3

--- Task3Script.R
+++ Task3Script.R
@@ -3,10 +3,17 @@
 # Author: Jonny
 ###############################################################################
 
-
-
 data2 <- read.csv("resources\\imports-85.data")
-summary(data2)
-attributes(data2)
+#summary(data2)
+#attributes(data2)
 
-princomp(data2, cor = TRUE)
+#x <- scale(as.numeric(data2$X3))
+#princomp(x)
+
+for(i in names(data2))
+{
+	repi <- noquote(paste("data2$",i, sep=""))
+	print(repi)
+	xi <- scale(as.numeric(repi))
+	princomp(xi)
+}
